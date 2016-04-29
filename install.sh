8e05af3f7dcdb7bb3a5f414cb3a8967abf9ec352
Roman Larionov <romanylarionov@gmail.com>
2016-04-29 09:06:29 -0400

    Turns out you need to actually download cuda sometimes.

--- install.sh
+++ install.sh
@@ -2,9 +2,12 @@
 
 cd ${HOME}
 
+sudo apt-get install git cmake gcc g++ curl
+curl -O http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda-repo-ubuntu1404-7-5-local_7.5-18_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
+rm cuda-repo-ubuntu1404_7.5-18_amd64.deb
 sudo apt-get update
-sudo apt-get install git cmake gcc g++ cuda
+sudo apt-get install cuda
 
 echo "export CUDA_HOME=/usr/local/cuda-7.5" >> ${HOME}/.bashrc
 echo "export LD_LIBRARY_PATH=${CUDA_HOME}"  >> ${HOME}/.bashrc
